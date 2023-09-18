0ca9e0f0b2145311f248faf0b8559e84e11603b7
Jason Woods <jason@coder.haus>
2023-09-18 06:45:18 -0500

    Add plotting function
    Show the movement of the weights by plotting the outputs.
    
    - Add matplotlib
    - Add plt setup
    - Move our Y value around a bit, to plot correctly
    - PLOT!

--- python/adv_perceptron.py
+++ python/adv_perceptron.py
@@ -1,7 +1,35 @@
 import random
+import matplotlib.pyplot as plt
+
+# plot vars
+color_list = ['r-', 'm-', 'y-', 'c-', 'b-', 'g-']
+color_index = 0
 
 def show_learning(w):
+    global color_index
+
     print('w0 = ', '%5.2f' % w[0], ', w1 = ' , '%5.2f' %w[1], ', w2 = ', '%5.2f' %w[2])
+
+    if color_index == 0:
+        plt.plot([1.0], [1.0], 'b_', markersize=12)
+        plt.plot([-1.0, 1.0, -1.0], [1.0, -1.0, -1.0], 'r+', markersize=12)
+        plt.axis([-2, 2, -2, 2])
+        plt.xlabel('x1')
+        plt.ylabel('x2')
+
+    x = [-2.0, 2.0]
+
+    if abs(w[2]) < 1e-5:
+        y = [-w[1]/(1e-5)*(-2.0)+(-w[0]/(1e-5)),
+             -w[1]/(1e-5)*((2.0)+(-w[0]/(1e-5)))]
+    else:
+        y = [-w[1]/w[2]*(x[0])+(-w[0]/w[2]),
+             -w[1]/w[2]*(x[1])+(-w[0]/w[2])]
+        
+    plt.plot(x, y, color_list[color_index])
+
+    if color_index < (len(color_list) - 1):
+        color_index += 1
 
 # define our hyperparams
 random.seed(7)
@@ -69,3 +97,4 @@ while not all_correct:
 
             show_learning(w)
 
+plt.show()
