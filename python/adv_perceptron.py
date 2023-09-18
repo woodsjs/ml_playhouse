beb89980bc77f1b1bf95da0153d1cdfe12a93693
Jason Woods <jason@coder.haus>
2023-09-18 05:51:23 -0500

    Add computation function
    - This is from the original perceptron code
    - Take in weights and variables
    - Compute output of signum function

--- python/adv_perceptron.py
+++ python/adv_perceptron.py
@@ -22,3 +22,17 @@ w = [0.2, -0.6, 0.25]
 #print initial weights
 show_learning(w)
 
+# First element in the vector v must be 1, our Bias value
+# Len of w, x is n+1 for n inputs
+
+def compute_output(w, v):
+    z = 0.0
+    
+    for i in range( len(w) ):
+        z += v[i] * w[i] # weighted sum of inputs
+    
+    # signum function
+    if z < 0: 
+        return -1
+    else:
+        return 1
