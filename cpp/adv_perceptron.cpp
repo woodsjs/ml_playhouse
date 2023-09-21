74133f0b7bc376f99fc34c0ee74476e9ef693217
Jason Woods <jason@coder.haus>
2023-09-21 06:16:47 -0500

    FInish main loop and add compute output
    - Add compute output function. This should use some nice std:: stuff but it doesn't.
    - Add actual calculation of weights based on y/y_train to our main loop. Did I mention we need to add rand and plotting still?

--- cpp/adv_perceptron.cpp
+++ cpp/adv_perceptron.cpp
@@ -2,12 +2,17 @@
 #include <random>
 #include <vector>
 
+// hey, don't judge!
+//  1. We'd probably use more goodies from std:: here but we're not (yet?)
+//  2. We're ultimately taking our python code and brute force swapping it to c++
+
 // we're not plotting here yet.
 // Yeah, the commented stuff is Python. You gotta problem with that?
 // # plot vars
 // color_list = ['r-', 'm-', 'y-', 'c-', 'b-', 'g-']
 // color_index = 0
 
+// TODO: pass by ref
 void show_learning(std::vector<double> w)
 {
     //     global color_index
@@ -35,6 +40,27 @@ void show_learning(std::vector<double> w)
     //         color_index += 1
 }
 
+int compute_output(std::vector<double> w, std::vector<double> v)
+{
+    auto z = 0.0;
+
+    for (auto i = 0; i < w.size(); i++)
+    {
+        // this would be better with std::reduce, accumulate, etc
+        z += v[i] * w[i]; // weighted sum of inputs
+    }
+
+    // signum function
+    if (z < 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+}
+
 int main()
 {
     // # define our hyperparams
@@ -47,68 +73,59 @@ int main()
     float LEARNING_RATE = 0.1;
     std::vector<int> index_list = {0, 1, 2, 3}; // will be used to randomize order
 
-    // #training examples.Ultimately, like the hyperparams, these should be passed in.
-    // #here we include in code as we write it
+    // training examples.Ultimately, like the hyperparams, these should be passed in.
+    // here we include in code as we write it
+    // First element in the vector v must be 1, our Bias value
+    // Len of w, x is n+1 for n inputs
     std::vector<std::vector<double>> x_train = {{1.0, -1.0, -1.0},
                                                 {1.0, -1.0, 1.0},
                                                 {1.0, 1.0, -1.0},
                                                 {1.0, 1.0, 1.0}};
     std::vector<double> y_train = {1.0, 1.0, 1.0, -1.0}; // ground truth
 
-    // #define perceptron weights. THese are arbitrarily chosen.
-    std::vector<double> w =  {0.2, -0.6, 0.25};
+    // define perceptron weights. THese are arbitrarily chosen.
+    std::vector<double> w = {0.2, -0.6, 0.25};
 
     // print initial weights
     show_learning(w);
+
+    // This is the meat and potatoes Perceptron training loop
+    bool all_correct = false;
+
+    while (!all_correct)
+    {
+        all_correct = true;
+
+        // TODO: get that rand working
+        // random.shuffle(index_list)
+
+        // take the randomized list vals in
+        for (auto i : index_list)
+        {
+            auto x = x_train[i];
+            auto y = y_train[i];
+
+            // run the perceptron function on each list item
+            auto p_out = compute_output(w, x);
+
+            // our output doesn't match our ground truth
+            if (y != p_out) // we're stopping at five for testing
+            {
+                // we're going to adjust the weights
+                for (auto j = 0; j < w.size(); j++)
+                {
+                    // take a weight from the list and add it to
+                    // our ground truth, multiplied by the learning rate, multiplied by
+                    // our matching input value
+                    // This moves our weight up or down, based on the ground truth's positive
+                    // or negative value
+                    w[j] += (y * LEARNING_RATE * x[j]);
+                }
+                all_correct = false;
+                show_learning(w);
+            }
+        }
+    }
+
+    // plt.show()
 }
-
-
-
-// # First element in the vector v must be 1, our Bias value
-// # Len of w, x is n+1 for n inputs
-
-// def compute_output(w, v):
-//     z = 0.0
-
-//     for i in range( len(w) ):
-//         z += v[i] * w[i] # weighted sum of inputs
-
-//     # signum function
-//     if z < 0:
-//         return -1
-//     else:
-//         return 1
-
-// # This is the meat and potatoes
-// # Perceptron training loop
-// all_correct = False
-
-// while not all_correct:
-//     all_correct = True
-//     random.shuffle(index_list)
-
-//     # take the randomized list vals in
-//     for i in index_list:
-//         x = x_train[i]
-//         y = y_train[i]
-
-//         # run the perceptron function on each list item
-//         p_out = compute_output(w, x)
-
-//         # our output doesn't match our ground truth
-//         if y != p_out:
-//             # we're going to adjust the weights
-//             for j in range(0, len(w)):
-
-//                 # take a weight from the list and add it to
-//                 # our ground truth, multiplied by the learning rate, multiplied by
-//                 # our matching input value
-//                 # This moves our weight up or down, based on the ground truth's positive
-//                 # or negative value
-//                 w[j] += (y * LEARNING_RATE * x[j])
-
-//             all_correct = False
-
-//             show_learning(w)
-
-// plt.show()
