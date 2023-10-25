2a6d13954fc6a942fc16863f32ccd7bb4609b1dd
Jason Woods <jason@coder.haus>
2023-10-25 05:30:27 -0500

    Add forward pass function
    - Calculate output of hidden and output layers

--- python/classify_handwritten_digits.py
+++ python/classify_handwritten_digits.py
@@ -20,7 +20,7 @@ def read_mnist():
     test_labels = idx2numpy.convert_from_file(TEST_LABEL_FILENAME)
 
     # flattens image to 1d instead of 2d
-    x_train = train_images.reshape(60000,784)
+    x_train = train_images.reshape(60000, 784)
     x_test = test_images.reshape(10000, 784)
 
     mean = np.mean(x_train)
@@ -32,8 +32,8 @@ def read_mnist():
     # this in particular gets us around 0
     # subtracting the mean, makes the mean 0 (if the mean is 1, and you subtract 1, you remove the mean)
     # dividing by the stddev, then scrunches the range in by that amount
-    x_train = (x_train - mean)/stddev
-    x_test = (x_test - mean)/stddev
+    x_train = (x_train - mean) / stddev
+    x_test = (x_test - mean) / stddev
 
     # one-hot
     # initialize everything to zero.  WE have 10 possible 1s for each set, since we're guessing
@@ -44,28 +44,31 @@ def read_mnist():
     # this looks at the training labels, which are index-value, and sets
     # one of the 10 possible 1s in y_train to 1
     # so if the label for index 3 is 6, it would set y_train[3][6] = 1, or 0000001000
-    for i,y in enumerate(train_labels):
+    for i, y in enumerate(train_labels):
         y_train[i][y] = 1
-    for i,y in enumerate(test_labels):
+    for i, y in enumerate(test_labels):
         y_test[i][y] = 1
-    
+
     return x_train, y_train, x_test, y_test
+
 
 x_train, y_train, x_test, y_test = read_mnist()
 index_list = list(range(len(x_train)))
+
 
 # Randomly initialize neuron weights from -1.0 to 1.0
 # This also leaves the bias weight (at pos 0), as a zero
 # We want to randomly intitialize to break up the symmetry. If the
 # weights are all the same number, they would all get the same adjustment during backprop.
 def layer_w(neuron_count, input_count):
-    weights = np.zeros((neuron_count, input_count+1))
+    weights = np.zeros((neuron_count, input_count + 1))
 
     for i in range(neuron_count):
-        for j in range(1, (input_count+1)):
+        for j in range(1, (input_count + 1)):
             weights[i][j] = np.random.uniform(-1.0, 1.0)
 
     return weights
+
 
 # 25 neurons, 784 inputs for our hidden layer
 hidden_layer_w = layer_w(25, 784)
@@ -85,6 +88,7 @@ chart_x = []
 chart_y_train = []
 chart_y_test = []
 
+
 # text representation of learning during one epoch
 # we store the data in global vars to use later
 def show_learning(epoch_no, train_acc, test_acc):
@@ -92,26 +96,44 @@ def show_learning(epoch_no, train_acc, test_acc):
     global chart_y_train
     global chart_y_test
 
-    print('epoch no: ', epoch_no, ', train_acc: ', '%6.4f' % train_acc,
-          ', test_acc: ', '%6.4f' % test_acc)
-    
+    print(
+        "epoch no: ",
+        epoch_no,
+        ", train_acc: ",
+        "%6.4f" % train_acc,
+        ", test_acc: ",
+        "%6.4f" % test_acc,
+    )
+
     chart_x.append(epoch_no + 1)
     chart_y_train.append(1.0 - train_acc)
     chart_y_test.append(1.0 - test_acc)
 
+
 def plot_learning():
-    plt.plot(chart_x, chart_y_train, 'r-', label='training error')
-    plt.plot(chart_x, chart_y_test, 'b-', label='test error')
+    plt.plot(chart_x, chart_y_train, "r-", label="training error")
+    plt.plot(chart_x, chart_y_test, "b-", label="test error")
 
     plt.axis([0, len(chart_x), 0, 1.0])
 
-    plt.xlabel('training epochs')
-    plt.ylabel('error')
+    plt.xlabel("training epochs")
+    plt.ylabel("error")
 
     plt.legend()
-    
-    plt.shor()
+
+    plt.show()
 
 
+def forward_pass(x):
+    global hidden_layer_y
+    global output_layer_y
 
+    for i, w in enumerate(hidden_layer_w):
+        z = np.dot(w, x)
+        hidden_layer_y[i] = np.tanh(z)
 
+    hidden_output_array = np.concatenate((np.array([1.0]), hidden_layer_y))
+
+    for i, w in enumerate(output_layer_w):
+        z = np.dot(w, hidden_output_array)
+        output_layer_y[i] = 1.0 / (1.0 + np.exp(-z))
