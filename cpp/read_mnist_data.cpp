7bb353c0f760e995a4fa2c0ea97167f3b1cdef62
Jason Woods <jason@coder.haus>
2023-11-04 07:33:47 -0500

    Finish data loader
    - Update read_images_labels to accept refs instead of returning vectors
    - Remove std::out stuff except for last data check
    - Correctly get file metadata
    - Build images vector with correct call to read
    - Print first example from the file

--- cpp/read_mnist_data.cpp
+++ cpp/read_mnist_data.cpp
@@ -1,52 +1,10 @@
-// import idx2numpy
-// #define idx2_Implementation
-// #include "idx2.hpp"
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <optional>
 #include <string>
+#include <sstream>
 #include <vector>
-
-// Where dez images
-// idx2::params TRAIN_IMAGE_P;
-// TRAIN_IMAGE_P.InputFile = "data/mnist/train-images.idx3-ubyte";
-// TRAIN_IMAGE_P.InDir = "data/mnist/";
-
-// idx2::idx2_file Idx2;
-// idx2_CleanUp(Dealloc(&Idx2)); // clean up Idx2 automatically in case of error
-// idx2::grid OutGrid = idx2::GetOutputGrid(Idx2, TRAIN_IMAGE_P);
-
-// idx2::buffer OutBuf;               // buffer to store the output
-// idx2_CleanUp(DeallocBuf(&OutBuf)); // deallocate OutBuf automatically in case of error
-// idx2::AllocBuf(&OutBuf, idx2::Prod<idx2::i64>(idx2::Dims(OutGrid)) * idx2::SizeOf(Idx2.DType));
-// idx2::Decode(&Idx2, TRAIN_IMAGE_P, &OutBuf);
-
-// std::string TRAIN_IMAGE_FILENAME = "data/mnist/train-images.idx3-ubyte";
-// std::string TRAIN_LABEL_FILENAME = "data/mnist/train-labels.idx1-ubyte";
-// std::string TEST_IMAGE_FILENAME = "data/mnist/t10k-images.idx3-ubyte";
-// std::string TEST_LABEL_FILENAME = "data/mnist/t10k-labels.idx1-ubyte";
-
-// train_images = idx2numpy.convert_from_file(TRAIN_IMAGE_FILENAME)
-// train_labels = idx2numpy.convert_from_file(TRAIN_LABEL_FILENAME)
-// test_images = idx2numpy.convert_from_file(TEST_IMAGE_FILENAME)
-// test_labels = idx2numpy.convert_from_file(TEST_LABEL_FILENAME)
-
-// print('Dimensions of train images ', train_images.shape)
-// print('Dimensions of train labels ', train_labels.shape)
-// print('Dimensions of test images ', test_images.shape)
-// print('Dimensions of test labels ', test_labels.shape)
-
-// # Show one training example
-// print('label for first training example ', train_labels[0])
-// print('---beginning of pattern for first training example---')
-// for line in train_images[0]:
-//     for num in line:
-//         if num > 0:
-//             print('*', end = ' ')
-//         else:
-//             print(' ', end = ' ')
-//     print('')
-// print('---end of pattern for first training exaple---')
 
 // None of this is me.  It's from https://www.kaggle.com/code/hojjatk/read-mnist-dataset/notebook
 class MnistDataloader
@@ -57,12 +15,12 @@ private:
     std::string LABEL_FILENAME = "../data/mnist/train-labels.idx1-ubyte";
 
 public:
-    void read_images_labels()
+    void read_images_labels(std::vector<uint16_t> &labels, std::vector<std::vector<std::vector<uint8_t>>> images)
     {
-        read_images_labels("", "");
+        read_images_labels("", "", labels, images);
     }
 
-    void read_images_labels(std::string input_image_filename, std::string input_label_filename)
+    void read_images_labels(std::string input_image_filename, std::string input_label_filename, std::vector<uint16_t> &labels, std::vector<std::vector<std::vector<uint8_t>>> &images)
     {
 
         if (!input_image_filename.empty())
@@ -98,7 +56,7 @@ public:
         }
 
         // to hold our labels, which are stored in 2 bytes
-        std::vector<uint16_t> labels = {};
+        // std::vector<uint16_t> labels = {};
         uint16_t label;
 
         while (labels_file.get(reinterpret_cast<char *>(&label), sizeof label))
@@ -109,7 +67,8 @@ public:
         labels_file.close();
 
         // read from the images file
-        std::ifstream images_file{IMAGE_FILENAME, images_file.binary | images_file.in};
+        // https://www.fon.hum.uva.nl/praat/manual/IDX_file_format.html
+        std::ifstream images_file{IMAGE_FILENAME, std::ios::binary | images_file.in};
         if (!images_file.is_open())
         {
             std::cout << "Failed to open " << IMAGE_FILENAME << std::endl;
@@ -120,90 +79,101 @@ public:
         int32_t images_magic_number;
 
         // reinterpret_cast is compile time, and can convert between pointer types.
+        // The sizes in each dimension are 4-byte integers (big endian, like in most non-Intel processors).
+        // so later we use the builtin_bswap32 to flipitty do
         images_file.read(reinterpret_cast<char *>(&images_magic_number), sizeof images_magic_number);
 
+        int32_t bswapped_images_magic_number = __builtin_bswap32(images_magic_number);
+
         // check the first two bytes of the magic number, should always be 00
-        if ((images_magic_number & 0x0000ffff) != 0)
+        if ((bswapped_images_magic_number & 0xffff0000) != 0)
         {
             std::cout << "Incorrect bytes in magic number for images file. Should be 0, but is " << (images_magic_number & 0xffff) << std::endl;
         }
 
-        // include gcc endian conversion with builtin_bswap32, since this is big endian
-        // std::cout << "First " << sizeof(images_magic_number) << " bytes of file " << __builtin_bswap32(images_magic_number) << std::endl;
-        if (__builtin_bswap32(images_magic_number) != 2051)
+        // make sure the magic number is good
+        if (bswapped_images_magic_number != 2051)
         {
-            std::cout << "Magic number mismatch. Expected 2051, but got " << __builtin_bswap32(images_magic_number) << std::endl;
+            std::cout << "Magic number mismatch. Expected 2051, but got " << bswapped_images_magic_number << std::endl;
             return;
         }
 
         // third byte is the data type. We really need to get this...
-        std::cout << "Datatype " << (__builtin_bswap32(images_magic_number) & 0x0000ff00) << std::endl;
+        // let's just check that it's 0800 hex, which is a byte
+        // then why not NOT do anything with it..lul
+        std::stringstream stream;
+        stream << std::setfill('0') << std::setw(sizeof(uint16_t) * 2)
+               << std::hex << (bswapped_images_magic_number & 0x0000ff00);
+        std::string datatype(stream.str());
+
+        auto datasize = 1;
+        if (datatype != "0800")
+        {
+            std::cout << "Datatype is not a byte sized one." << std::endl;
+        }
 
         // fourth byte is the number of dimensions
         // we can make the rest of below more generic by using this to get the sizes of each dimension
-        std::cout << "Number of dimensions " << (__builtin_bswap32(images_magic_number) & 0x000000ff) << std::endl;
+        // now let's get the overall size
+        uint32_t num_images;
+        // images_file.read(reinterpret_cast<char *>(&num_images), sizeof num_images);
+        images_file.read(reinterpret_cast<char *>(&num_images), sizeof num_images);
+        uint32_t bswapped_num_images = __builtin_bswap32(num_images);
 
-        // now let's get the row/col data
-        uint32_t images_rows;
-        images_file.read(reinterpret_cast<char *>(&images_rows), sizeof images_rows);
-        std::cout << "Number of rows " << __builtin_bswap32(images_rows) << std::endl;
+        // col size
+        uint32_t num_image_cols;
+        images_file.read(reinterpret_cast<char *>(&num_image_cols), sizeof num_image_cols);
+        uint32_t bswapped_num_image_cols = __builtin_bswap32(num_image_cols);
 
-        uint32_t images_cols;
-        images_file.read(reinterpret_cast<char *>(&images_cols), sizeof images_cols);
-        std::cout << "Number of columns " << __builtin_bswap32(images_cols) << std::endl;
+        // rows in each image
+        uint32_t num_image_rows;
+        images_file.read(reinterpret_cast<char *>(&num_image_rows), sizeof num_image_rows);
+        uint32_t bswapped_num_image_rows = __builtin_bswap32(num_image_rows);
 
-        uint32_t images_depth;
-        images_file.read(reinterpret_cast<char *>(&images_depth), sizeof images_depth);
-        std::cout << "Depth of data " << __builtin_bswap32(images_depth) << std::endl;
+        auto images_full_data_size = bswapped_num_image_cols * bswapped_num_image_rows * bswapped_num_images;
 
-        auto images_full_data_size = images_rows * images_cols * images_depth;
+        // so our images are stored in 8 byte chunks, 28 rows x 28 cols for each image
+        uint8_t value;
+        std::vector<uint8_t> row;
+        std::vector<std::vector<uint8_t>> image;
+        // std::vector<std::vector<std::vector<uint8_t>>> images;
 
-        // to hold our images, which are stored in 2 bytes
-        // std::vector<uint16_t> images = {};
-        // uint16_t image;
+        for (auto h = 0; h < bswapped_num_images; h++)
+        {
+            image.clear();
+            for (auto i = 0; i < bswapped_num_image_rows; i++)
+            {
+                row.clear();
+                for (auto j = 0; j < bswapped_num_image_cols; j++)
+                {
+                    images_file.read(reinterpret_cast<char *>(&value), sizeof value);
 
-        // while (images_file.get(reinterpret_cast<char *>(&image), sizeof image))
-        // {
-        //     labels.push_back(label);
-        // }
+                    row.push_back(value);
+                }
+                image.push_back(row);
+            }
+            images.push_back(image);
+        }
 
-        //             image_data = array("B", file.read())
-        //         images = []
-        //         for i in range(size):
-        //             images.append([0] * rows * cols)
-        //         for i in range(size):
-        //             img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
-        //             img = img.reshape(28, 28)
-        //             images[i][:] = img
+        std::cout << "Label for training example " << labels[0] << std::endl;
+        std::cout << "---beginning of pattern for training example---" << std::endl;
+        for (auto row : images[0])
+        {
+            for (auto num : row)
+            {
+                std::cout << (num > 0 ? "*" : " ") << " ";
+            }
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
 
-        labels_file.close();
+        std::cout << "---end of pattern for training example---" << std::endl;
+
+        images_file.close();
 
         //         return images, labels
     }
 };
-
-//     def read_images_labels(self, images_filepath, labels_filepath):
-//         labels = []
-//         with open(labels_filepath, 'rb') as file:
-//             magic, size = struct.unpack(">II", file.read(8))
-//             if magic != 2049:
-//                 raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
-//             labels = array("B", file.read())
-
-//         with open(images_filepath, 'rb') as file:
-//             magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
-//             if magic != 2051:
-//                 raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
-//             image_data = array("B", file.read())
-//         images = []
-//         for i in range(size):
-//             images.append([0] * rows * cols)
-//         for i in range(size):
-//             img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
-//             img = img.reshape(28, 28)
-//             images[i][:] = img
-
-//         return images, labels
 
 //     def load_data(self):
 //         x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
@@ -213,9 +183,10 @@ public:
 int main()
 {
     MnistDataloader mnistDL;
-    mnistDL.read_images_labels();
-    std::cout << "blah"
-              << "blah" << std::endl;
+    std::vector<uint16_t> labels;
+    std::vector<std::vector<std::vector<uint8_t>>> images;
+
+    mnistDL.read_images_labels(labels, images);
 
     return 1;
 }
