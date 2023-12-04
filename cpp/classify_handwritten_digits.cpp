b7e2c57162f7f34eca25509657bb35398d9e2a5a
Jason Woods <jason@coder.haus>
2023-12-04 05:36:38 -0600

    Finished up for now

--- cpp/classify_handwritten_digits.cpp
+++ cpp/classify_handwritten_digits.cpp
@@ -12,7 +12,7 @@
 
 // np.random.seed(7)
 const float LEARNING_RATE = 0.01;
-const int EPOCHS = 5;
+const int EPOCHS = 20;
 
 // # Where dez images
 std::string TRAIN_IMAGE_FILENAME = "../data/mnist/train-images.idx3-ubyte";
@@ -88,7 +88,7 @@ void forward_pass(std::vector<double> &x, std::vector<double> &hidden_layer_y, s
 		double z;
 
 		for ( auto j = 0; j < hidden_layer_weights[i].size(); j++) {
-			z += x[i]*hidden_layer_weights[i][j];
+			z += x[j]*hidden_layer_weights[i][j];
 		}
       	
 		hidden_layer_y[i] = std::tanh(z);
@@ -106,14 +106,14 @@ void forward_pass(std::vector<double> &x, std::vector<double> &hidden_layer_y, s
 	
 		double z;
 		for ( auto j = 0; j < output_layer_weights[i].size(); j++) {
-		  z += hidden_output_array[i]*output_layer_weights[i][j];
+		  z += hidden_output_array[j]*output_layer_weights[i][j];
 		}
  
 	output_layer_y[i] = 1.0 / (1.0 + std::exp(-z));
     }
 }
 
-void backward_pass(std::vector<uint16_t> &y_truth, std::vector<double> &hidden_layer_error, std::vector<double> &output_layer_error, std::vector<double> &output_layer_y, std::vector<double> &hidden_layer_y, std::vector<std::vector<double>> &output_layer_weights)
+void backward_pass(std::vector<double> &y_truth, std::vector<double> &hidden_layer_error, std::vector<double> &output_layer_error, std::vector<double> &output_layer_y, std::vector<double> &hidden_layer_y, std::vector<std::vector<double>> &output_layer_weights)
 {
 
     //     # backproping error
@@ -129,7 +129,7 @@ void backward_pass(std::vector<uint16_t> &y_truth, std::vector<double> &hidden_l
     for (auto i = 0; i < hidden_layer_y.size(); i++)
     {
         double error_weights;
-
+		
         double derivative = 1.0 - pow(hidden_layer_y[i], 2); 
 							    
 		for ( auto j = 0; j < output_layer_weights.size(); j++ ) {
@@ -156,16 +156,16 @@ void adjust_weights(std::vector<double> &x, std::vector<double> &hidden_layer_er
     // add intermediate results destroy
 
     // add in our bias term!
-    //std::vector<std::vector<double>> hidden_output_array(hidden_layer_y.size(), std::vector<double>(2,0.0));
-    //std::transform(hidden_layer_y.begin(), hidden_layer_y.end(), hidden_output_array.begin(),[](auto x) -> std::vector<double> {return {1.0, x};});
+   std::vector<double> hidden_output_array(hidden_layer_y.size() + 1, 0.0);
+   hidden_output_array.insert(hidden_output_array.begin(), hidden_layer_y.begin(), hidden_layer_y.end());
+   hidden_output_array.insert(hidden_output_array.begin(), 1.0);
 
    for ( auto i = 0; i < output_layer_error.size(); i++ ) { // 10 rows
 	   for ( auto j = 0; j < output_layer_weights[i].size(); j++) { // 10 rows of 25 cols
 			// hidden output array is 25 rows, 2 cols
 			// output_layer_error is 10 rows
 		
-		   output_layer_weights[i][j] -= ( 1.0 * LEARNING_RATE * output_layer_error[i]);
-		   output_layer_weights[i][j] -= ( hidden_layer_y[j] * LEARNING_RATE * output_layer_error[i]);
+		   output_layer_weights[i][j] -= ( hidden_output_array[j] * LEARNING_RATE * output_layer_error[i]);
 	   }
    }
 
@@ -179,38 +179,56 @@ int main(void)
     auto output_neuron_count = 10;
 
     // our mnist reader will push out our labels (y_train, y_test), as well as our images (x_train, x_test)
-    std::vector<uint16_t> y_train;
+    std::vector<uint16_t> train_labels;
     std::vector<std::vector<std::vector<uint8_t>>> x_train;
     
-    std::vector<uint16_t> y_test;
+    std::vector<uint16_t> test_labels;
     std::vector<std::vector<std::vector<uint8_t>>> x_test;
 
-    read_mnist(TRAIN_IMAGE_FILENAME, TRAIN_LABEL_FILENAME, y_train, x_train);
-    read_mnist(TEST_IMAGE_FILENAME, TEST_LABEL_FILENAME, y_test, x_test);
+    read_mnist(TRAIN_IMAGE_FILENAME, TRAIN_LABEL_FILENAME, train_labels, x_train);
+    read_mnist(TEST_IMAGE_FILENAME, TEST_LABEL_FILENAME, test_labels, x_test);
+
+	// to test our guessed values to our labels, we take the labels returned 
+	// which are ints, and use that as the index to a vector that represents
+	// the predicted value
+	// So if the label is 3, we set a vector of 0,0,0,1,0,0,0,0,0,0
+	// to match our predicted vector
+	std::vector<std::vector<double>> y_train(x_train.size(), std::vector<double>(output_neuron_count, 0.0));
+	std::vector<std::vector<double>> y_test(x_test.size(), std::vector<double>(output_neuron_count, 0.0));
+
+	for ( auto i = 0; i < train_labels.size(); i++) {
+			y_train[i][train_labels[i]] = 1.0;
+	}
+
+	for ( auto i = 0; i < test_labels.size(); i++) {
+			y_test[i][test_labels[i]] = 1.0;
+	}
 
     // flatten the x_train, x_tests
     std::vector<std::vector<uint8_t>> flat_x_train;
    
     for (auto i = 0; i < x_train.size(); i++)
     {
-	std::vector<uint8_t> temp_image;
+		std::vector<uint8_t> temp_image;
 
-	for (auto j = 0; j < x_train[i].size(); j++){	
+		for (auto j = 0; j < x_train[i].size(); j++){	
             temp_image.insert(temp_image.end(), x_train[i][j].begin(), x_train[i][j].end());
-	}
-	flat_x_train.push_back(temp_image);
+		}
+
+		flat_x_train.push_back(temp_image);
+
     }
 
     std::vector<std::vector<uint8_t>> flat_x_test;
    
     for (auto i = 0; i < x_test.size(); i++)
     {
-	std::vector<uint8_t> temp_image;
+		std::vector<uint8_t> temp_image;
 
-	for (auto j = 0; j < x_test[i].size(); j++){	
+		for (auto j = 0; j < x_test[i].size(); j++){	
             temp_image.insert(temp_image.end(), x_test[i][j].begin(), x_test[i][j].end());
-	}
-	flat_x_test.push_back(temp_image);
+		}
+		flat_x_test.push_back(temp_image);
     }
 
     // Set up the ability to normalize the data
@@ -244,10 +262,6 @@ int main(void)
     // index_list = list(range(len(x_train)))
     // std::vector<int> index_list; 
 
-    // get our weights
-    // std::vector<std::vector<double>> neuron_weights;
-    // neuron_weights = init_neuron_weights(3, 24);
-
     // # 25 neurons, 784 inputs for our hidden layer
     std::vector<std::vector<double>> hidden_layer_weights = init_neuron_weights(hidden_neuron_count, hidden_layer_inputs);
     // 25neurons gives us 25 outputs, init to zero
@@ -276,7 +290,6 @@ int main(void)
 
         // # don't really like that we're using global vars
         // # below doesn't give a good feel as to what's going on for a programmer
-        std::cout << "Training set size " << x_train_normalized.size() << std::endl;
         for (auto j = 0; j < x_train_normalized.size(); j++)
         {
             if (j % 10000 == 0)
@@ -287,34 +300,42 @@ int main(void)
             {
                 std::cout << "*";
             }
-	    
-            forward_pass(x_train_normalized[j], hidden_layer_y, output_layer_y, hidden_layer_weights, output_layer_weights);
+	   		
+		   	std::vector<double> x;
+			x.insert(x.begin(), x_train_normalized[j].begin(), x_train_normalized[j].end());
+			x.insert(x.begin(), 1.0);
+			
+            forward_pass(x, hidden_layer_y, output_layer_y, hidden_layer_weights, output_layer_weights);
 
             std::vector<double>::iterator result;
             result = std::max_element(output_layer_y.begin(), output_layer_y.end());
+
             int max_y = std::distance(output_layer_y.begin(), result);
 
-            if (max_y == y_train[j])
+            if (max_y == train_labels[j])
             {
                 correct_training_results += 1;
             }
-            backward_pass(y_train, hidden_layer_error, output_layer_error, output_layer_y, hidden_layer_y, output_layer_weights);
+            backward_pass(y_train[j], hidden_layer_error, output_layer_error, output_layer_y, hidden_layer_y, output_layer_weights);
 
-            adjust_weights(x_train_normalized[j], hidden_layer_error, output_layer_error, hidden_layer_weights, hidden_layer_y, output_layer_weights);
+            adjust_weights(x, hidden_layer_error, output_layer_error, hidden_layer_weights, hidden_layer_y, output_layer_weights);
 
         }
         std::cout << std::endl;
         auto correct_test_results = 0;
         for (auto k = 0; k < x_test_normalized.size(); k++)
         {
+			std::vector<double> x;
+			x.insert(x.begin(), x_test_normalized[k].begin(), x_test_normalized[k].end());
+			x.insert(x.begin(), 1.0);
 
-            forward_pass(x_test_normalized[k], hidden_layer_y, output_layer_y, hidden_layer_weights, output_layer_weights);
+            forward_pass(x, hidden_layer_y, output_layer_y, hidden_layer_weights, output_layer_weights);
 
             std::vector<double>::iterator result;
             result = std::max_element(output_layer_y.begin(), output_layer_y.end());
             int max_y = std::distance(output_layer_y.begin(), result);
 
-            if (max_y == y_test[k])
+            if (max_y == test_labels[k])
             {
                 correct_test_results += 1;
             }
