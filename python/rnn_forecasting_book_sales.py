334cc0ccf0225d455dd5d369adc4d1f1f09df463
Jason Woods <jason@coder.haus>
2024-01-08 06:36:54 -0600

    Start for the RNN time series solution. - Add timeseries data from https://www.census.gov/econ/currentdata/ - Add python file and add includes.

---
+++ python/rnn_forecasting_book_sales.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import SimpleRNN
+
+import logging
+
+
