b68f3914098700dbd88b5db9534a19804c9fece8
Jason Woods <jason@coder.haus>
2024-01-11 05:39:47 -0600

    CLosing up, moving south.

--- python/rnn_forecasting_book_sales.py
+++ python/rnn_forecasting_book_sales.py
@@ -63,6 +63,17 @@ def naivePrediction(test_sales):
     plt.legend()
     plt.show()
 
+def naiveStandardizedPrediction(test_sales_std):
+    test_output = test_sales_std[MIN:]
+    naive_prediction = test_sales_std[MIN-1:-1]
+
+    mean_squared_error = np.mean(np.square(naive_prediction - test_output))
+    mean_abs_error = np.mean(np.abs(naive_prediction - test_output))
+
+    print('naive test mse: ', mean_squared_error)
+    print('naive test mean abs: ', mean_abs_error)
+
+
 sales = readfile(FILE_NAME)
 
 months = len(sales)
@@ -73,3 +84,49 @@ test_sales = sales[split:]
 
 plotDataset(sales)
 naivePrediction(test_sales)
+
+# standardize it
+# be sure to use only the training set
+mean = np.mean(train_sales)
+stddev = np.std(train_sales)
+train_sales_std = (train_sales - mean) / stddev
+test_sales_std = (test_sales - mean) / stddev
+
+train_months = len(train_sales)
+train_X = np.zeros((train_months-MIN, train_months-1,1))
+train_y = np.zeros((train_months-MIN, 1))
+
+for i in range(0, train_months-MIN):
+    train_X[i, -(i+MIN):, 0] = train_sales_std[0:i+MIN]
+    train_y[i, 0] = train_sales_std[i+MIN]
+
+# and now test
+mean = np.mean(test_sales)
+stddev = np.std(test_sales)
+test_sales_std = (test_sales - mean) / stddev
+test_sales_std = (test_sales - mean) / stddev
+
+test_months = len(test_sales)
+test_X = np.zeros((test_months-MIN, test_months-1,1))
+test_y = np.zeros((test_months-MIN, 1))
+
+for i in range(0, test_months-MIN):
+    test_X[i, -(i+MIN):, 0] = test_sales_std[0:i+MIN]
+    test_y[i, 0] = test_sales_std[i+MIN]
+
+# model
+model = Sequential()
+model.add(SimpleRNN(128, activation='relu',
+            input_shape=(None, 1)))
+model.add(Dense(1, activation='linear'))
+model.compile(loss='mean_squared_error', optimizer='adam',
+            metrics=['mean_absolute_error'])
+
+model.summary()
+
+history = model.fit(train_X, train_y,
+        validation_data = (test_X, test_y), epochs=EPOCHS,
+        batch_size=BATCH_SIZE, verbose=2,
+        shuffle=True)
+
+naiveStandardizedPrediction(test_sales_std)
