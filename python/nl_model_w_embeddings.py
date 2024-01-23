363ebb99672fae3435acb1192fe21b61a61ca1d7
Jason Woods <jason@coder.haus>
2024-01-23 16:00:48 -0600

    Start embedding model. - Get text and split - Create embeddings

---
+++ python/nl_model_w_embeddings.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.text import text_to_word_sequence
+
+import tensorflow as tf
+
+import logging
+
+tf.get_logger().setLevel(logging.ERROR)
+
+EPOCHS = 32
+BATCH_SIZE = 256
+INPUT_FILE_NAME = '../data/frankenstein.txt'
+WINDOW_LENGTH = 40
+WINDOW_STEP = 3
+PREDICT_LENGTH = 3
+MAX_WORDS = 10000
+EMBEDDING_WIDTH = 100
+
+file = open(INPUT_FILE_NAME, 'r', encoding='utf-8-sig')
+text = file.read()
+file.close()
+
+text = text_to_word_sequence(text)
+
+fragments = []
+targets = []
+
+for i in range(0, len(text) - WINDOW_LENGTH, WINDOW_STEP):
+    fragments.append(text[i: i + WINDOW_LENGTH])
+    targets.append(text[i + WINDOW_LENGTH])
+
+# we'll only tokenize 10000 words. If a word isn't found, 'UNK' is used
+tokenizer = Tokenizer(num_words=MAX_WORDS, oov_token='UNK')
+# assign indexes to words
+tokenizer.fit_on_texts(text)
+
+# convert each fragment into indicies
+fragments_indexed = tokenizer.texts_to_sequences(fragments)
+targets_indexed = tokenizer.texts_to_sequences(targets)
+
+X = np.array(fragments_indexed, dtype=np.int64)
+y = np.zeros((len(targets_indexed), MAX_WORDS))
+
+for i, target_index in enumerate(targets_indexed):
+    y[i, target_index] = 1
+
+
