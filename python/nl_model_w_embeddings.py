40570b8d1feb8a3bd63e4749bebac3cde4796ab1
Jason Woods <jason@coder.haus>
2024-01-24 09:54:07 -0600

    Get stuff right. - Predict next three words. - Realize you're using a newer version of tensorflow and downgrade. - Realize you didn't up your epochs. - ??? - Profit!

--- python/nl_model_w_embeddings.py
+++ python/nl_model_w_embeddings.py
@@ -13,7 +13,7 @@ import logging
 
 tf.get_logger().setLevel(logging.ERROR)
 
-EPOCHS = 32
+EPOCHS = 32 
 BATCH_SIZE = 256
 INPUT_FILE_NAME = '../data/frankenstein.txt'
 WINDOW_LENGTH = 40
@@ -50,29 +50,89 @@ y = np.zeros((len(targets_indexed), MAX_WORDS))
 for i, target_index in enumerate(targets_indexed):
     y[i, target_index] = 1
 
+# Build and train model.
 training_model = Sequential()
+
 training_model.add(Embedding(
-    output_dim=EMBEDDING_WIDTH,
-    input_dim=MAX_WORDS,
-    mask_zero=True,
-    input_length=None))
-training_model.add(LSTM(128, 
-    return_sequences=True,
-    dropout=0.2,
-    recurrent_dropout=0.2))
-training_model.add(LSTM(128, 
-    dropout=0.2,
-    recurrent_dropout=0.2))
+    output_dim=EMBEDDING_WIDTH, input_dim=MAX_WORDS,
+    mask_zero=True, input_length=None))
+training_model.add(LSTM(128, return_sequences=True,
+                        dropout=0.2))
+training_model.add(LSTM(128, dropout=0.2))
 training_model.add(Dense(128, activation='relu'))
 training_model.add(Dense(MAX_WORDS, activation='softmax'))
 
-training_model.compile(loss='categorical_crossentropy', optimizer='adam')
+training_model.compile(loss='categorical_crossentropy',
+                       optimizer='adam')
 
 training_model.summary()
 
 history = training_model.fit(X, y, validation_split=0.05,
-        batch_size=BATCH_SIZE,
-        epochs=EPOCHS,
-        verbose=2,
-        shuffle=True)
- 
+                             batch_size=BATCH_SIZE,
+                             epochs=EPOCHS, verbose=2,
+                             shuffle=True)
+
+# Build stateful model used for prediction.
+inference_model = Sequential()
+
+inference_model.add(Embedding(
+    output_dim=EMBEDDING_WIDTH, input_dim=MAX_WORDS,
+    mask_zero=True, batch_input_shape=(1,1)))
+inference_model.add(LSTM(128, return_sequences=True,
+                         dropout=0.2,
+                         stateful=True))
+inference_model.add(LSTM(128, dropout=0.2,
+                         stateful=True))
+inference_model.add(Dense(128, activation='relu'))
+inference_model.add(Dense(MAX_WORDS, activation='softmax'))
+
+weights = training_model.get_weights()
+
+inference_model.set_weights(weights)
+
+# Provide beginning of sentence and
+# predict next words in a greedy manner
+first_words = ['i', 'saw']
+first_words_indexed = tokenizer.texts_to_sequences(
+    first_words)
+inference_model.reset_states()
+predicted_string = ''
+# Feed initial words to the model.
+for i, word_index in enumerate(first_words_indexed):
+    x = np.zeros((1, 1), dtype=np.int64)
+    x[0][0] = word_index[0]
+    predicted_string += first_words[i]
+    predicted_string += ' '
+    y_predict = inference_model.predict(x, verbose=0)[0]
+# Predict PREDICT_LENGTH words.
+for i in range(PREDICT_LENGTH):
+    new_word_index = np.argmax(y_predict)
+    word = tokenizer.sequences_to_texts(
+        [[new_word_index]])
+    x[0][0] = new_word_index
+    predicted_string += word[0]
+    predicted_string += ' '
+    y_predict = inference_model.predict(x, verbose=0)[0]
+print(predicted_string)
+
+# Explore embedding similarities.
+embeddings = training_model.layers[0].get_weights()[0]
+lookup_words = ['the', 'saw', 'see', 'of', 'and',
+                'monster', 'frankenstein', 'read', 'eat']
+for lookup_word in lookup_words:
+    lookup_word_indexed = tokenizer.texts_to_sequences(
+        [lookup_word])
+    print('words close to:', lookup_word)
+    lookup_embedding = embeddings[lookup_word_indexed[0]]
+    word_indices = {}
+    # Calculate distances.
+    for i, embedding in enumerate(embeddings):
+        distance = np.linalg.norm(
+            embedding - lookup_embedding)
+        word_indices[distance] = i
+    # Print sorted by distance.
+    for distance in sorted(word_indices.keys())[:5]:
+        word_index = word_indices[distance]
+        word = tokenizer.sequences_to_texts([[word_index]])[0]
+        print(word + ': ', distance)
+    print('')
