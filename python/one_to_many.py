260aef3a193d559365a87772e9c835b52af095be
Jason Woods <jason@coder.haus>
2024-02-07 06:58:02 -0600

    Process image data. - Read in each image, get vector and pickle. - Read out all data for later use.

--- python/one_to_many.py
+++ python/one_to_many.py
@@ -40,3 +40,54 @@ model_new = Model(inputs=model.input,
 model_new.summary()
 
 
+# get them outputs
+for i, key in enumerate(image_dict.keys()):
+    if i % 1000 == 0:
+        print('Progress: ' + str(i) + ' images processed')
+
+    item = image_dict.get(key)
+    filename = TRAINING_FILE_DIR + 'train2014/' + item[0]
+
+    # get dimensions
+    image = load_img(filename)
+    width = image.size[0]
+    height = image.size[1]
+
+    # resize
+    if height > width:
+        image = load_img(filename, target_size=(int(height/width*256), 256))
+    else:
+        image = load_img(filename, target_size=(256, int(height/width*256)))
+
+
+    width = image.size[0]
+    height = image.size[1]
+    image_np = image_to_array(image)
+
+    # crop image, leaving inner bits
+    h_start = int((height-224)/2)
+    w_start = int((width-224)/2)
+
+    image_np = image_np[h_start:h_start+224,
+            w_start:w_start+224]
+
+    # increase dimenstions
+    image_np = np.expand_dims(image_np, axis=0)
+
+    # call model and save tensor to disk
+    X = preprocess_input(image_np)
+    y = model_new.predict(X)
+
+    save_filename = OUTPUT_FILE_DIR + item[0] + '.pickle.gzip'
+
+    pickle_file = gzip.open(sae_filename, 'wb')
+    pickle.dump(y[0], pickle_file)
+    pickle_file.close()
+
+# save dict with captions and filenames
+save_filename = OUTPUT_FILE_DIR + 'caption_file.pickle.gz'
+
+pickle_file = gzip.open(save_filename, 'wb')
+pickle.dump(image_dict, pickle_file)
+pickle_file.close()
+
