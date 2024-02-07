50ccbdde5515cf1d9f50300b89acb3f1da8677a6
Jason Woods <jason@coder.haus>
2024-02-07 06:14:51 -0600

    Get image data and annotations

--- python/one_to_many.py
+++ python/one_to_many.py
@@ -18,4 +18,14 @@ tf.get_logger().setLevel(logging.ERROR)
 TRAINING_FILE_DIR = '../data/coco/'
 OUTPUT_FILE_DIR = 'tf_data/feature_vectors/'
 
+with open(TRAINING_FILE_DIR + 'captions_train2014.json') as json_file:
+    data = json.load(json_file)
+
+image_dict = {}
+
+for image in data['images']:
+    image_dict[image['id']] = [image['file_name']]
+
+for anno in data['annotations']:
+    image_dict[anno['image_id']].append(anno['caption'])
 
